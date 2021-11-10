9bc82d8c9be3768d0b6fd4189bee788120fb3f8f
deboraconstantino <45357007+deboraconstantino@users.noreply.github.com>
2021-11-09 23:08:46 -0300

    Seção 4: Desafio Composição

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -1,11 +1,24 @@
 const fs = require('fs')
 const path = require('path')
 
+function composicao(...fns) {
+    return function(valor) {
+        return fns.reduce(async (acc, fn) => {
+            if(Promise.resolve(acc) === acc) {
+                return fn(await acc)
+            } else {
+                return fn(acc)
+            }
+        }, valor)
+    }
+}
+
 function lerDiretorio(caminho) {
     return new Promise((resolve, reject) => {
         try {
-            let arquivos = fs.readdirSync(caminho)
-            arquivos = arquivos.map(arquivo => path.join(caminho, arquivo))
+            const arquivos = fs.readdirSync(caminho).map(arquivo => {
+                return path.join(caminho, arquivo)
+            })
             resolve(arquivos)
         } catch (e) {
             reject(e)
@@ -13,10 +26,10 @@ function lerDiretorio(caminho) {
     })
 }
 
-function lerArquivo(arquivo) {
+function lerArquivo(caminho) {
     return new Promise((resolve, reject) => {
         try {
-            const conteudo = fs.readFileSync(arquivo, {encoding: 'utf-8'})
+            const conteudo = fs.readFileSync(caminho, { encoding: 'utf-8' })
             resolve(conteudo.toString())
         } catch (e) {
             reject(e)
@@ -28,24 +41,26 @@ function lerArquivos(caminhos) {
     return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
 }
 
-function elementosTerminadosCom(array, padrao) {
-    return array.filter(el => el.endsWith(padrao))
-}
-
-function removerSeVazio(array) {
-    return array.filter(el => el.trim())
-}
-
-function removerSeIncluir(padrao) {
-    return function(array) {
-        return array.filter(el => !el.includes(padrao))
+function elementosTerminadosCom(padraoTextual) {
+    return function (array) {
+        return array.filter(el => el.endsWith(padraoTextual))
     }
 }
 
-function removerSeApenasNumero(array) {
+function removerElementosSeVazio(array) {
+    return array.filter(el => el.trim())
+}
+
+function removerElementosSeIncluir(padraoTextual) {
+    return function (array) {
+        return array.filter(el => !el.includes(padraoTextual))
+    }
+}
+
+function removerElementosSeApenasNumero(array) {
     return array.filter(el => {
         const num = parseInt(el.trim())
-        return num !== num // dá certo quando o num não é convertido, obtendo o valor NaN
+        return num !== num
     })
 }
 
@@ -59,6 +74,16 @@ function removerSimbolos(simbolos) {
     }
 }
 
+function mesclarElementos(array) {
+    return array.join(' ')
+}
+
+function separarTextoPor(simbolo) {
+    return function (texto) {
+        return texto.split(simbolo)
+    }
+}
+
 function agruparElementos(palavras) {
     return Object.values(palavras.reduce((acc, palavra) => {
         const el = palavra.toLowerCase()
@@ -68,22 +93,26 @@ function agruparElementos(palavras) {
     }, {}))
 }
 
-function ordenarPorAtributoNumerico(atributo, ordem = 'asc') {
-    return function(array) {
-        const asc = (o1, o2) => o1[atributo] - o2[atributo]
-        const desc = (o1, o2) => o2[atributo] - o1[atributo]
-        return array.sort(ordem === 'asc' ? asc : desc)
+function ordernarPorAtribNumerico(attr, ordem = 'asc') {
+    return function (array) {
+        const asc = (o1, o2) => o1[attr] - o2[attr]
+        const desc = (o1, o2) => o2[attr] - o1[attr]
+        return [...array].sort(ordem === 'asc' ? asc : desc)
     }
 }
 
 module.exports = {
+    composicao,
     lerDiretorio,
-    elementosTerminadosCom,
+    lerArquivo,
     lerArquivos,
-    removerSeVazio,
-    removerSeIncluir,
-    removerSeApenasNumero,
+    elementosTerminadosCom,
+    removerElementosSeVazio,
+    removerElementosSeIncluir,
+    removerElementosSeApenasNumero,
     removerSimbolos,
+    mesclarElementos,
+    separarTextoPor,
     agruparElementos,
-    ordenarPorAtributoNumerico
+    ordernarPorAtribNumerico
 }
