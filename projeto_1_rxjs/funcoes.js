028fedff4fb0d1e90c7bc28a0cf59132700714f5
deboraconstantino <45357007+deboraconstantino@users.noreply.github.com>
2021-11-18 21:14:18 -0300

    Seção 6: Migrando o código para RxJS

--- projeto_1_rxjs/funcoes.js
+++ projeto_1_rxjs/funcoes.js
@@ -1,118 +1,125 @@
 const fs = require('fs')
 const path = require('path')
-
-function composicao(...fns) {
-    return function(valor) {
-        return fns.reduce(async (acc, fn) => {
-            if(Promise.resolve(acc) === acc) {
-                return fn(await acc)
-            } else {
-                return fn(acc)
-            }
-        }, valor)
-    }
-}
+const { Observable } = require('rxjs')
 
 function lerDiretorio(caminho) {
-    return new Promise((resolve, reject) => {
+    return new Observable(subscriber => {
         try {
-            const arquivos = fs.readdirSync(caminho).map(arquivo => {
-                return path.join(caminho, arquivo)
+            fs.readdirSync(caminho).forEach(arquivo => {
+                subscriber.next(path.join(caminho, arquivo))
             })
-            resolve(arquivos)
+            subscriber.complete()
         } catch (e) {
-            reject(e)
+            subscriber.error(e)
         }
     })
 }
 
-function lerArquivo(caminho) {
-    return new Promise((resolve, reject) => {
-        try {
-            const conteudo = fs.readFileSync(caminho, { encoding: 'utf-8' })
-            resolve(conteudo.toString())
-        } catch (e) {
-            reject(e)
+function lerArquivo() {
+    return createPipeableOperator(subscriber => ({
+        next(caminho) {
+            try {
+                const conteudo = fs.readFileSync(caminho, {
+                    encoding: 'utf-8'
+                })
+                subscriber.next(conteudo.toString())
+            } catch (e) {
+                subscriber.error()
+            }
         }
-    })
-}
-
-function lerArquivos(caminhos) {
-    return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
+    }))
 }
 
 function elementosTerminadosCom(padraoTextual) {
-    return function (array) {
-        return array.filter(el => el.endsWith(padraoTextual))
-    }
+    return createPipeableOperator(subscriber => ({
+        next(texto) {
+            if (texto.endsWith(padraoTextual)) {
+                subscriber.next(texto)
+            }
+        }
+    }))
 }
 
-function removerElementosSeVazio(array) {
-    return array.filter(el => el.trim())
+function removerElementosSeVazio() {
+    return createPipeableOperator(subscriber => ({
+        next(texto) {
+            if (texto.trim()) {
+                subscriber.next(texto)
+            }
+        }
+    }))
 }
 
-function removerElementosSeIncluir(padraoTextual) {
-    return function (array) {
-        return array.filter(el => !el.includes(padraoTextual))
-    }
+function removerElementosSeApenasNumero() {
+    return createPipeableOperator(subscriber => ({
+        next(texto) {
+            const num = parseInt(texto.trim())
+            if (num !== num) {
+                subscriber.next(texto)
+            }
+        }
+    }))
 }
 
-function removerElementosSeApenasNumero(array) {
-    return array.filter(el => {
-        const num = parseInt(el.trim())
-        return num !== num
-    })
-}
 
 function removerSimbolos(simbolos) {
-    return function (array) {
-        return array.map(el => {
-            return simbolos.reduce((acc, simbolo) => {
-                return acc.split(simbolo).join('')
-            }, el)
+    return createPipeableOperator(subscriber => ({
+        next(texto) {
+            const textoSemSimbolos = simbolos.reduce(
+                (acc, simbolo) => {
+                    return acc.split(simbolo).join('')
+                }
+                , texto)
+            subscriber.next(textoSemSimbolos)
+        }
+    }))
+}
+
+function separarTextoPor(simbolo) {
+    return createPipeableOperator(subscriber => ({
+        next(texto) {
+            texto.split(simbolo).forEach(parte => {
+                subscriber.next(parte)
+            })
+        }
+    }))
+}
+
+function agruparElementos() {
+    return createPipeableOperator(subscriber => ({
+        next(palavras) {
+            const agrupado = Object.values(
+                palavras.reduce((acc, palavra) => {
+                    const el = palavra.toLowerCase()
+                    const qtde = acc[el] ? acc[el].qtde + 1 : 1
+                    acc[el] = { elemento: el, qtde }
+                    return acc
+                }, {}))
+            subscriber.next(agrupado)
+        }
+    }))
+}
+
+function createPipeableOperator(operatorFn) {
+    return function (source) {
+        return Observable.create(subscriber => {
+            const sub = operatorFn(subscriber)
+            source.subscribe({
+                next: sub.next,
+                error: sub.error || (e => subscriber.error(e)),
+                complete: sub.complete || (e => subscriber.complete(e)),
+            })
         })
     }
 }
 
-function mesclarElementos(array) {
-    return array.join(' ')
-}
-
-function separarTextoPor(simbolo) {
-    return function (texto) {
-        return texto.split(simbolo)
-    }
-}
-
-function agruparElementos(palavras) {
-    return Object.values(palavras.reduce((acc, palavra) => {
-        const el = palavra.toLowerCase()
-        const qtde = acc[el] ? acc[el].qtde + 1 : 1
-        acc[el] = { elemento: el, qtde }
-        return acc
-    }, {}))
-}
-
-function ordernarPorAtribNumerico(attr, ordem = 'asc') {
-    return function (array) {
-        const asc = (o1, o2) => o1[attr] - o2[attr]
-        const desc = (o1, o2) => o2[attr] - o1[attr]
-        return [...array].sort(ordem === 'asc' ? asc : desc)
-    }
-}
-
 module.exports = {
-    composicao,
     lerDiretorio,
     lerArquivo,
-    lerArquivos,
     elementosTerminadosCom,
     removerElementosSeVazio,
-    removerElementosSeIncluir,
     removerElementosSeApenasNumero,
     removerSimbolos,
-    mesclarElementos,
     separarTextoPor,
     agruparElementos,
-    ordernarPorAtribNumerico
 }
